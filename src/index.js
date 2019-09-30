87c90f66c5468ed120f28bcf828c2310d197fb25
vadimkirov <vadimkirov@yandex.ru>
2019-09-30 16:32:41 +0300

    ready

--- src/index.js
+++ src/index.js
@@ -39,6 +39,27 @@ const MORSE_TABLE = {
 
 function decode(expr) {
     // write your solution here
+    let result='';
+    for(let count = 0; count < expr.length; count = count + 10) {
+        let daChar = expr.substr(count, 10);
+
+        if (daChar === '**********') {
+            result += ' ';
+            continue;
+        }
+        let findChar='';
+        for (let i = 0; i < daChar.length; i = i + 2) {
+           let j = daChar[i]+daChar[i+1];
+
+            if(j === '10'){
+            findChar +='.'}
+            if(j === '11'){
+                findChar +='-'}
+        }
+        result += MORSE_TABLE[findChar];
+
+    }
+    return result;
 }
 
 module.exports = {
